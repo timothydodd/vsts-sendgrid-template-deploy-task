d6efab415bf9a835c515fcc5522f7f43b09b0764
Timothy Dodd <tdodd@viewfi.com>
2022-09-14 17:10:52 -0400

    Added Catch that will Fail the Task
    - moved eslintrc file

--- task/index.ts
+++ task/index.ts
@@ -1,6 +1,7 @@
 import * as client from '@sendgrid/client';
 import { ClientRequest } from '@sendgrid/client/src/request';
 import * as taskLibrary from "azure-pipelines-task-lib/task";
+import { TaskResult } from 'azure-pipelines-task-lib/task';
 import * as fs from 'fs';
 
 interface ILogger {
@@ -80,46 +81,51 @@ class Logger implements ILogger {
 let logger: ILogger;
 
 async function run() {
-    const sendGridApiKey = taskLibrary.getInput('sendGridApiKey');
-    const filePath = taskLibrary.getPathInput('templateFile', true, true);
-    const templateId = taskLibrary.getInput('templateId');
-    const versionId = taskLibrary.getInput('versionId');
-    const logLevel = taskLibrary.getInput('logLevel');
-
-    logger = new Logger(Logger.parseLoglevel(logLevel));
-
-    logger.info(`TemplateId:${templateId}`);
-    logger.info(`VersionId:${versionId}`);
-
-    client.setApiKey(sendGridApiKey);
-
-    const content = fs.readFileSync(filePath, 'utf-8');
-
-    logger.info(`Template Content Length: ${content.length}`);
-
-    if (!content || content.length === 0) {
-        logger.warn('Template Deploy Aborted because no content found')
-        return;
-    }
-    const data = {
-        "template_id": templateId,
-        "html_content": content,
-        "generate_plain_content": false,
-    };
-    const request = {
-        url: `/v3/templates/${templateId}/versions/${versionId}`,
-        method: 'PATCH',
-        headers: null,
-        body: data
-    } as ClientRequest
-
-
     try {
-        const response = await client.request(request);
-        logger.info(`SendGrid Response Code: ${response[0].statusCode}`);
+        const sendGridApiKey = taskLibrary.getInput('sendGridApiKey');
+        const filePath = taskLibrary.getPathInput('templateFile', true, true);
+        const templateId = taskLibrary.getInput('templateId');
+        const versionId = taskLibrary.getInput('versionId');
+        const logLevel = taskLibrary.getInput('logLevel');
 
-    } catch (error) {
-        logger.error(error);
+        logger = new Logger(Logger.parseLoglevel(logLevel));
+
+        logger.info(`TemplateId:${templateId}`);
+        logger.info(`VersionId:${versionId}`);
+
+        client.setApiKey(sendGridApiKey);
+
+        const content = fs.readFileSync(filePath, 'utf-8');
+
+        logger.info(`Template Content Length: ${content.length}`);
+
+        if (!content || content.length === 0) {
+            logger.warn('Template Deploy Aborted because no content found')
+            return;
+        }
+        const data = {
+            "template_id": templateId,
+            "html_content": content,
+            "generate_plain_content": false,
+        };
+        const request = {
+            url: `/v3/templates/${templateId}/versions/${versionId}`,
+            method: 'PATCH',
+            headers: null,
+            body: data
+        } as ClientRequest
+
+
+        try {
+            const response = await client.request(request);
+            logger.info(`SendGrid Response Code: ${response[0].statusCode}`);
+
+        } catch (error) {
+            logger.error(error);
+        }
+    }
+    catch (err) {
+        taskLibrary.setResult(TaskResult.Failed, err.message || 'run() failed', true);
     }
 }
 
